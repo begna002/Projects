4fd62fbd1875148828cc782870f19a4cb16c73b0
begna002 <begna002@umn.edu>
2019-01-05 12:49:10 -0600

    adding new data sets, deleting old ones

--- NCAA_Model/Past_Matches.py
+++ NCAA_Model/Past_Matches.py
@@ -1,31 +1,43 @@
 import pandas as pd
 import sys
-data = pd.read_csv("NCAATourneyDetailedResultsEnriched2018.csv")
-teams = pd.read_csv("Teams.csv")
+
+data = pd.read_csv("Big_Dance_CSV.csv")
 
 d = {'this_team' :[], 'other_team' : [], 'won' :[], 'year' :[], 'score': []}
-TEAMS = {}
 
-for i in range(len(teams.TeamID)):
-    TEAMS[teams.TeamID[i]] = teams.TeamName[i]
+for i in range(len(data.Year)):
+    if data.Score1[i] > data.Score2[i]:
+        d['this_team'].append(data.Team1[i])
+        d['this_team'].append(data.Team2[i])
+        
+        d['other_team'].append(data.Team2[i])
+        d['other_team'].append(data.Team1[i])
+        
+        d['won'].append('Win')
+        d['won'].append('Lose')
+        
+        d['year'].append(data.Year[i])
+        d['year'].append(data.Year[i])
+        
+        d['score'].append((data.Score1[i], data.Score2[i]))
+        d['score'].append((data.Score2[i], data.Score1[i]))
+    else:
+        d['this_team'].append(data.Team2[i])
+        d['this_team'].append(data.Team1[i])
+        
+        d['other_team'].append(data.Team1[i])
+        d['other_team'].append(data.Team2[i])
+        
+        d['won'].append('Win')
+        d['won'].append('Lose')
+        
+        d['year'].append(data.Year[i])
+        d['year'].append(data.Year[i])
+        
+        d['score'].append((data.Score2[i], data.Score1[i]))
+        d['score'].append((data.Score1[i], data.Score2[i]))
 
-for i in range(len(data.WTeamID)):
-    d['this_team'].append(TEAMS.get(data.WTeamID[i]))
-    d['this_team'].append(TEAMS.get(data.LTeamID[i]))
-    
-    d['other_team'].append(TEAMS.get(data.LTeamID[i]))
-    d['other_team'].append(TEAMS.get(data.WTeamID[i]))
-
-    d['won'].append('Win')
-    d['won'].append('Lose')
-
-    d['year'].append(data.Season[i])
-    d['year'].append(data.Season[i])
-
-    d['score'].append((data.WScore[i], data.LScore[i]))
-    d['score'].append((data.LScore[i], data.WScore[i]))
-
-
+        
 def all_matches():
     for i in range(len(d['this_team'])):
         print("This Team:", d['this_team'][i], "| Other Team:", d['other_team'][i], "| Won/Lost: ", d['won'][i], "| Year:", d['year'][i])
@@ -49,7 +61,7 @@ def Match_Up(team1, team2):
                 years_lost.append(d['year'][i])
                 losing_score.append(d['score'][i])
     if num_won != 0 or num_lost != 0:
-        print("Since 2003", team1, "has won", num_won, "times, and lost", num_lost, "times against", team2, "in the NCAA Tournament")
+        print("Since 1985", team1, "has won", num_won, "times, and lost", num_lost, "times against", team2, "in the NCAA Tournament")
 
         if len(years_won) > 0:
             print("")
@@ -61,14 +73,14 @@ def Match_Up(team1, team2):
             print(years_lost, losing_score)
         
     else:
-        print("Since 2003,", team1, "hasn't had a match-up against", team2, "in the NCAA Tournament (Check Spelling)")
+        print("Since 1985,", team1, "hasn't had a match-up against", team2, "in the NCAA Tournament (Check Spelling)")
 
 
 
 def year_Champ(year):
     print("")
     for i in range(len(d['year'])):
-        if year == 2017:
+        if year == 2018:
             print(year, "NCAA CHAMPION:", d['other_team'][-1])
             print("Played against:", d['this_team'][-1])
             print("Score:", d['score'][-2])
@@ -85,13 +97,13 @@ def history(team):
     print("")
     last = False
     found = False
-    if team == 'North Carolina':
+    if team == 'Villanova':
         last = True
     for i in range(len(d['this_team'])):
         if d['this_team'][i] == team and d['won'][i] == 'Win':
             found = True
             print("Year:", d['year'][i], "| Match against:", d['other_team'][i], "| Outcome: ", d['won'][i], "| Score:", d['score'][i])
-            if d['year'][i] != 2017:
+            if d['year'][i] != 2018:
                 if (d['year'][i+2]) == (d['year'][i] + 1):
                     champion.append(d['year'][i])
                     final_loser.append(d['other_team'][i])
@@ -105,18 +117,18 @@ def history(team):
         for i in range(len(champion)):
             print(champion[i], "against", final_loser[i])
     if last == True:
-        print("2017 against Gonzaga")
+        print("2018 against Michigan")
     if found == False:
-        print(team, "has not played in the NCAA tournament since 2003")
+        print(team, "has not played in the NCAA tournament since 1985")
 
 
 def helpmsg():
     print("Options:")
-    print(" --Type Winner Year to get Champion of given year (from 2003-2017)")
-    print(" --Type all to get all NCAA Tournament match results (from 2003-2017)")
-    print(" --Type Team to get results of this team's past matches of NCAA Tournament (from 2003-2017)")
-    print(" --Type Team 1 Team 2 to get results of NCAA Tournament matches (from 2003-2017)")
-    print(" --NOTE: If Team is 2 words, combine with ''")
+    print(" --Type Winner Year to get Champion of given year (from 1985-2018)")
+    print(" --Type all to get all NCAA Tournament match results (from 1985-2018)")
+    print(" --Type Team Name to get results of this team's past matches of NCAA Tournament (from 1985-2018)")
+    print(" --Type Team 1 Name Team 2 Name to get results of NCAA Tournament matches (from 1985-2018)")
+    print(" --NOTE: If Team Name is greater than 2 words, combine with ''")
     print(" --Type exit to end program")
     print(" --Type help at any time to get options")
 
